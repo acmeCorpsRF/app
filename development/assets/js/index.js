3f746c20c46c88cc31f8e9c94bfed9817b20ead3
ivanov.n <acme.corps.rf@gmail.com>
2019-08-26 18:23:49 +0300

    day 26.08

--- development/assets/js/index.js
+++ development/assets/js/index.js
@@ -1,1 +1,23 @@
-console.log('Welcome to production');
+import $ from 'jQuery';
+
+$(function () {
+    let sum = (...args) => args.reduce((m, n) => m + n, 0);
+    $('p').text(sum());
+    console.log(process.env.NODE_ENV) + '\n';
+
+    let promise = new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            reject(new Error("время вышло!"));
+        }, 1000);
+
+    });
+
+    promise
+        .then(
+            result => alert("Fulfilled: " + result),
+            error => alert("Rejected: " + error.message)
+        );
+
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,62 @@
-"use strict";
+'use strict';
 
 const path = require('path');
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    context: path.resolve(__dirname, 'app'),
+    mode: NODE_ENV,
     entry: './development/assets/js/index.js',
     output: {
         path: path.resolve(__dirname, 'public/js'),
         filename: 'index.js',
-        publicPath: 'public/'
+        publicPath: 'public/js'
     },
-    watch: NODE_ENV == 'development',
-    devtool: NODE_ENV == 'development' ? 'cheap-inline-module-source-map' : null,
+    watch: NODE_ENV === 'development',
+    // devtool: NODE_ENV == 'development' ? 'cheap-module-source-map' : false,
+    devtool: NODE_ENV == 'development' ? 'inline-cheap-module-source-map' : false,
+    devServer: {
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
+            }
+        ]
+    },
     plugins: [
-        new webpack.EnvironmentPlugin(NODE_ENV)
+        new webpack.DefinePlugin({
+            NODE_ENV: NODE_ENV
+        }),
+        new UglifyJsPlugin({
+            test: /\.js(\?.*)?$/i,
+            uglifyOptions: {
+                compress: {
+                    unsafe: true,
+                    inline: true,
+                    passes: 2,
+                    keep_fargs: false,
+                },
+                output: {
+                    beautify: false,
+                },
+                mangle: true,
+            }
+        })
     ]
+
 };
