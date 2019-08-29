74074b39a11754a84ceccc621ab9b637043b72ee
ivanov.n <acme.corps.rf@gmail.com>
2019-08-29 18:35:39 +0300

    day 29.08

--- development/assets/js/pages/main/index.js
+++ development/assets/js/pages/main/index.js
@@ -4,22 +4,12 @@ import $ from 'jQuery';
 
 $(function () {
     let sum = (...args) => args.reduce((m, n) => m + n, 0);
-    $('p').text(sum());
     $('body').append('<div>');
+    $('div').text(sum());
     console.log(process.env.NODE_ENV) + '\n';
 
-    let promise = new Promise((resolve, reject) => {
-
-        setTimeout(() => {
-            reject(new Error("Время вышло! asdfasdasda"));
-        }, 1000);
-
-    });
-
-    promise
-        .then(
-            result => alert("Fulfilled: " + result),
-            error => alert("Rejected: " + error.message)
-        );
+    setTimeout(() => {
+        new Error("Страница обновилась!");
+    }, 2000);
 
 });
--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,37 @@
 
 const path = require('path');
 const webpack = require('webpack');
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const PATH = {
+    absolute: path.resolve(__dirname, ''),
+    pages: path.resolve(__dirname, './development/pages'),
+    assets: path.resolve(__dirname, './development/assets'),
+    public: path.resolve(__dirname, './public')
+}
 
 module.exports = {
-    context: path.resolve(__dirname, './development/assets/js/pages/'),
+    context: path.resolve(__dirname, ''),
     mode: NODE_ENV,
     entry: {
-        main: './main/index.js'
+        main: `${PATH.assets}/js/pages/main/index.js`
     },
     output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'js/[name].[chunkhash].js',
+        path: `${PATH.public}`,
+        // filename: 'js/[name].[chunkhash].js',
+        filename: 'js/[name].js',
         publicPath: 'public'
     },
     watch: NODE_ENV === 'development',
-    // devtool: NODE_ENV == 'development' ? 'cheap-module-source-map' : false,
-    devtool: NODE_ENV == 'development' ? 'inline-cheap-module-source-map' : false,
+    devtool: NODE_ENV == 'development' ? 'cheap-module-source-map' : false,
+    // devtool: NODE_ENV == 'development' ? 'cheap-module-eval-source-map' : false,
+    // devtool: NODE_ENV == 'development' ? 'cheap-module-inline-source-map' : false,
     devServer: {
-        overlay: true
+        overlay: true,
+        contentBase: `${PATH.absolute}`
     },
     module: {
         rules: [
@@ -39,19 +49,19 @@ module.exports = {
                     {
                         loader: MiniCssExtractPlugin.loader,
                         options: {
-                            sourceMap: NODE_ENV == 'development' ? true : false
+                            // sourceMap: NODE_ENV == 'development' ? true : false
                         }
                     },
                     {
                         loader: 'css-loader',
                         options: {
-                            sourceMap: NODE_ENV == 'development' ? true : false
+                            // sourceMap: NODE_ENV == 'development' ? true : false
                         }
                     },
                     {
                         loader: 'postcss-loader',
                         options: {
-                            sourceMap: NODE_ENV == 'development' ? true : false,
+                            // sourceMap: NODE_ENV == 'development' ? true : false,
                             config: {
                                 path: 'development/assets/js/postcss.config.js'
                             }
@@ -60,7 +70,7 @@ module.exports = {
                     {
                         loader: 'sass-loader',
                         options: {
-                            sourceMap: NODE_ENV == 'development' ? true : false
+                            // sourceMap: NODE_ENV == 'development' ? true : false
                         }
                     }
 
@@ -88,7 +98,17 @@ module.exports = {
             }
         }),
         new MiniCssExtractPlugin({
-            filename: 'css/styles.[contenthash].css',
+            // filename: 'css/styles.[contenthash].css',
+            filename: 'css/styles.css',
+        }),
+        new HtmlWebpackPlugin({
+            hash: false,
+            template: `${PATH.pages}/index.html`,
+            title: 'app',
+            filename: `${PATH.absolute}/index.html`
+        }),
+        new webpack.SourceMapDevToolPlugin({
+            filename: NODE_ENV == 'development' ? 'js/[name].js.map' : false
         }),
         new ProgressBarPlugin()
     ]
